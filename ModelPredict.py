0d6dd1ea5c1066aee141749ebf5d9520e1a08210
apple <apple@appledeMacBook-Pro.local>
2019-01-15 23:19:50 +0100

    added a file for prediction of user's data image

--- ModelPredict.py
+++ ModelPredict.py
@@ -1,2 +1,14 @@
 import numpy as np
 import cv2
+from keras.models import Model,load_model
+
+model = load_model('ResNet50.h5')
+#model.get_weigts()
+input_image = cv2.imread('InputImage.jpg')
+
+def predict(img):
+    pred = model.predict(img)
+    print("Prediction of the input image: " + str(pred))
+    return pred
+
+prediction = predict(input_image)
