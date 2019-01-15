a2876cc143d4fb3767e31d035450369dd27052ee
apple <apple@appledeMacBook-Pro.local>
2019-01-15 23:16:50 +0100

    finished datapreprocessing(include split of train,dev,test set, normalization etc.)

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -7,58 +7,63 @@ from time import time
 from time import sleep
 from scipy import ndimage
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-#define folder directory and file locations
-"""
-folder = "EczemaHands"
-onlyfiles = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-#define train files
-train_files = []
-i=0
-for _file in onlyfiles:
-    train_files.append(_file)
 
-print("Files in train_files: %d" % len(train_files))
+X_train = np.load('dataset/X_train.npy')
+Y_train = np.load('dataset/Y_train.npy')
+X_dev = np.load('dataset/X_dev.npy')
+Y_dev = np.load('dataset/Y_dev.npy')
+X_test = np.load('dataset/X_test.npy')
+Y_test = np.load('dataset/Y_test.npy')
 
-channels = 3
-image_height = 128
-image_width = 128
 
-dataset_diseased_hand = np.ndarray(shape=(len(train_files), image_height, image_width, channels),
-                     dtype=np.float32)
-for _file in train_files:
-    try:
-        img = load_img(folder + "/" + _file)  # this is a PIL image
-        # Convert to Numpy Array
-        x = img_to_array(img)
-        x = x.reshape(128,128,3)
-        # Normalize
-        dataset_diseased_hand[i] = x
-        i += 1
-        if i % 250 == 0:
-            print("%d images to array" % i)
-    except(ValueError,OSError):
-        pass
-
-np.save("diseased_images.npy", dataset_diseased_hand)
-print("All images to array!")
-"""
 #Train:915 CV:233 test:16
-DiseasedHand = np.load('EczemaHandDataset.npy')
-m = DiseasedHand.shape[0]
-
+ImageArray = np.load('dataset/normalhand.npy')
+m = ImageArray.shape[0]
+print('normal hand m: ' + str(m))
 permutation = list(np.random.permutation(m))
-X = DiseasedHand[permutation,:,:,:]
+X = ImageArray[permutation,:,:,:]
 
-Diseased_X_train = X[0:915,:,:,:] #input of training set m*n
-Diseased_Y_train = np.ones((915))
+SecondImageArray = np.load('dataset/EczemaHandDataset.npy')
+m2 = SecondImageArray.shape[0]
+print('EczemaHand dataset shape: ' + str(m2))
+print(SecondImageArray.shape)
 
-Diseased_X_dev = X[915:1148,:,:,:] #
-diseased_Y_dev = np.ones((233)) #
-diseased_X_test = X[1148:,:,:,:]
-diseased_Y_test = np.ones((16))
+#function that splits datasets into three sets
+def split_into_three_sets(dataset,TrainSetNum,CVSetNum,TestSetNum,classes):
+    X_train = dataset[0:TrainSetNum,:,:,:] #input of training set m*n
+    Y_train = np.ones((TrainSetNum))
+    X_dev = dataset[TrainSetNum:TrainSetNum+CVSetNum,:,:,:] #
+    Y_dev = np.ones((CVSetNum)) #
+    X_test = dataset[TrainSetNum+CVSetNum:,:,:,:]
+    Y_test = np.ones((TestSetNum))
+    return(X_train,Y_train,X_dev,Y_dev,X_test,Y_test)
 
-print("X_train shape:" + str(Diseased_X_train.shape))
-print("X_dev shape: " + str(Diseased_X_dev.shape))
-print("Y_train shape:" + str(Diseased_Y_train.shape))
-print("Y_dev shape" + str(diseased_Y_dev.shape))
-print("X_test shape:" + str(diseased_X_test.shape))
+X_train2,Y_train2,X_dev2,Y_dev2,X_test2,Y_test2 = split_into_three_sets(ImageArray,1342,447,447,2)
+
+X_train = np.append(X_train,X_train2,axis = 0)
+X_dev  = np.append(X_dev,X_dev2,axis = 0)
+X_test = np.append(X_test,X_test2,axis = 0)
+Y_train = np.append(Y_train,Y_train2,axis = 0)
+Y_dev = np.append(Y_dev,Y_dev2,axis = 0)
+Y_test = np.append(Y_test,Y_test2,axis = 0)
+
+
+print("X_train shape:" + str(X_train.shape))
+print("X_dev shape: " + str(X_dev.shape))
+print("X_test shape:" + str(X_test.shape))
+print("Y_train shape:" + str(Y_train.shape))
+print("Y_dev shape" + str(Y_dev.shape))
+print("Y_test shape:" + str(Y_test.shape))
+
+print(Y_train)
+print(Y_dev)
+print(Y_test)
+"""
+np.save('dataset/X_train.npy',X_train)
+np.save('dataset/Y_train.npy',Y_train)
+np.save('dataset/X_dev.npy',X_dev)
+np.save('dataset/Y_dev.npy',Y_dev)
+np.save('dataset/X_test.npy',X_test)
+np.save('dataset/Y_test.npy',Y_test)
+
+"""
