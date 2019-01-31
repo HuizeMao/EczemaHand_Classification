b677d94edd2b5a71dbda50e2931c59cdc71c662b
apple <apple@appledeMacBook-Pro.local>
2019-01-31 20:50:27 +0100

    changed architecture, +1 conv layer, and drop out

--- ModelTraining.py
+++ ModelTraining.py
@@ -1,6 +1,7 @@
 #import all necessary libraries
 import numpy as np
 import keras
+from keras.losses import binary_crossentropy
 from keras import layers
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, Dropout
 from keras.models import Model, load_model
@@ -24,7 +25,7 @@ import cv2
 X_train_orig, Y_train, X_CV_orig, Y_CV, X_test_orig, Y_test = load_dataset()
 
 #select a training data image and show
-show_img = (X_train_orig[0]).astype(np.uint8)
+show_img = (X_train_orig[290]).astype(np.uint8)
 print(show_img)
 plt.imshow(show_img)
 plt.show()
@@ -80,13 +81,13 @@ def identity_block(X, f, filters, stage, block):
     X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
-    X = Dropout(0.2)(X)
+    X = Dropout(0.5)(X)
 
     # Second component of main path (≈3 lines)
     X = Conv2D(filters = F2, kernel_size = (f,f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
     X = Activation('relu')(X)
-    X = Dropout(0.2)(X)
+    X = Dropout(0.5)(X)
 
     # Third component of main path (≈2 lines)
     X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
@@ -95,7 +96,7 @@ def identity_block(X, f, filters, stage, block):
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = Add()([X,X_shortcut])
     X = Activation('relu')(X)
-    X = Dropout(0.2)(X)
+    X = Dropout(0.5)(X)
 
     return X
 
@@ -173,40 +174,45 @@ def ResNet50(input_shape,classes):
     X_input = Input(input_shape)
 
     # Zero-Padding
-    X = ZeroPadding2D((3, 3))(X_input)
+    X = ZeroPadding2D((7, 7))(X_input)
 
     # Stage 1
-    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1')(X)
+    X = Conv2D(64, (5, 5), strides = (2, 2), name = 'conv1', padding ='same')(X)
     X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
     X = Activation('relu')(X)
-    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
-
+    X = MaxPooling2D((3, 3), strides=(2,2))(X)
     # Stage 2
-    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
-    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
-    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+    X = Conv2D(128, (3, 3), strides = (2, 2), padding = 'same', name = 'conv2')(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv2')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)
 
     # Stage 3
-    X = convolutional_block(X, f = 3, filters = [128,128,512], stage = 3, block='a', s = 2)
-    X = identity_block(X, 3, [128,128,512], stage=3, block='b')
-    X = identity_block(X, 3, [128,128,512], stage=3, block='c')
-    X = identity_block(X, 3, [128,128,512], stage=3, block='d')
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=3, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=3, block='c')
 
     # Stage 4
-    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
-    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
-    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
-    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
-    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
-    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+    X = convolutional_block(X, f = 3, filters = [128,128,512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128,128,512], stage=4, block='b')
+    X = identity_block(X, 3, [128,128,512], stage=4, block='c')
+    X = identity_block(X, 3, [128,128,512], stage=4, block='d')
 
     # Stage 5
-    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
-    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
-    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='f')
 
-    # AVGPOOL
-    X = AveragePooling2D(pool_size=(2,2), name = 'avg_pool')(X)
+    # Stage 6
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=6, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=6, block='c')
+
+    # Maxpool
+    X = MaxPooling2D(pool_size=(2,2), name = 'max_pool')(X)
 
     # output layer
     X = Flatten()(X)
@@ -228,10 +234,10 @@ model = ResNet50((128,128,3),1)
 #compile
 model.compile(loss='binary_crossentropy',
             optimizer = keras.optimizers.SGD(lr=0.01, decay=0, momentum=0, nesterov=False),
-            metrics=['acc',binary_accuracy])
+            metrics=['acc',binary_crossentropy])
 
 #fit
-history = model.fit(X_train, Y_train, batch_size = m,epochs = 10,verbose = 1, validation_data = (X_CV,Y_CV),shuffle=True)
+history = model.fit(X_train, Y_train, batch_size = 2315,epochs = 10,verbose = 1, validation_data = (X_CV,Y_CV),shuffle=True)
 #save model
 model.save('ResNet50_4.h5')
 
