71ee99e92901df89d90ae1ed08f4a3a44ecac8f7
apple <apple@appledeMacBook-Pro.local>
2019-02-02 20:37:20 +0100

    initialize training for inception model

---
+++ Inception-v4ModelTraining.py
@@ -0,0 +1,305 @@
+import numpy as np
+# Sys
+import warnings
+# Keras Core
+import keras
+from keras.layers.convolutional import MaxPooling2D, Convolution2D, AveragePooling2D
+from keras.layers import Input, Dropout, Dense, Flatten, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import concatenate
+from keras import regularizers
+from keras import initializers
+from keras.models import Model
+# Backend
+from keras.callbacks import ModelCheckpoint,EarlyStopping
+from keras import backend as K
+# Utils
+from keras.utils.layer_utils import convert_all_kernels_in_model
+from keras.utils.data_utils import get_file
+from resnets_utils import *
+import scipy.misc
+from matplotlib.pyplot import imshow
+from matplotlib import pyplot as plt
+
+#load data
+X_train_orig, Y_train, X_CV_orig, Y_CV, X_test_orig, Y_test = load_dataset()
+
+#select a training data image and show
+show_img = (X_train_orig[490]).astype(np.uint8)
+print(show_img)
+plt.imshow(show_img)
+plt.show()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_CV = X_CV_orig/255.
+X_test = X_test_orig/255.
+
+m = X_train[0].shape
+print("Number of training examples: {}".format(m))
+#change the shape of Y outputs
+Y_train = np.expand_dims(Y_train,axis=1)
+Y_CV = np.expand_dims(Y_CV,axis=1)
+Y_test = np.expand_dims(Y_test,axis=1)
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of cross validation examples = " + str(X_CV.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("X_CV shape: " + str(X_CV.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("Y_CV shape: " + str(Y_CV.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+input = input('pause')
+
+
+def conv2d_bn(x, nb_filter, num_row, num_col,
+              padding='same', strides=(1, 1), use_bias=False):
+    channel_axis = -1
+    x = Convolution2D(nb_filter, (num_row, num_col),
+                      strides=strides,
+                      padding=padding,
+                      use_bias=use_bias,
+                      kernel_regularizer=regularizers.l2(0.00004),
+                      kernel_initializer=initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='normal', seed=None))(x)
+    x = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(x)
+    x = Activation('relu')(x)
+    return x
+
+
+def block_inception_a(input):
+    channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 96, 1, 1)
+
+    branch_1 = conv2d_bn(input, 64, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 96, 3, 3)
+
+    branch_2 = conv2d_bn(input, 64, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
+    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
+
+    branch_3 = AveragePooling2D((3,3), strides=(1,1), padding='same')(input)
+    branch_3 = conv2d_bn(branch_3, 96, 1, 1)
+
+    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
+    return x
+
+
+def block_reduction_a(input):
+    channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 384, 3, 3, strides=(2,2), padding='valid')
+
+    branch_1 = conv2d_bn(input, 192, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 224, 3, 3)
+    branch_1 = conv2d_bn(branch_1, 256, 3, 3, strides=(2,2), padding='valid')
+
+    branch_2 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(input)
+
+    x = concatenate([branch_0, branch_1, branch_2], axis=channel_axis)
+    return x
+
+
+def block_inception_b(input):
+    channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 384, 1, 1)
+
+    branch_1 = conv2d_bn(input, 192, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 224, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 256, 7, 1)
+
+    branch_2 = conv2d_bn(input, 192, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 192, 7, 1)
+    branch_2 = conv2d_bn(branch_2, 224, 1, 7)
+    branch_2 = conv2d_bn(branch_2, 224, 7, 1)
+    branch_2 = conv2d_bn(branch_2, 256, 1, 7)
+
+    branch_3 = AveragePooling2D((3,3), strides=(1,1), padding='same')(input)
+    branch_3 = conv2d_bn(branch_3, 128, 1, 1)
+
+    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
+    return x
+
+
+def block_reduction_b(input):
+    channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 192, 1, 1)
+    branch_0 = conv2d_bn(branch_0, 192, 3, 3, strides=(2, 2), padding='valid')
+
+    branch_1 = conv2d_bn(input, 256, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 256, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 320, 7, 1)
+    branch_1 = conv2d_bn(branch_1, 320, 3, 3, strides=(2,2), padding='valid')
+
+    branch_2 = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(input)
+
+    x = concatenate([branch_0, branch_1, branch_2], axis=channel_axis)
+    return x
+
+
+def block_inception_c(input):
+    channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 256, 1, 1)
+
+    branch_1 = conv2d_bn(input, 384, 1, 1)
+    branch_10 = conv2d_bn(branch_1, 256, 1, 3)
+    branch_11 = conv2d_bn(branch_1, 256, 3, 1)
+    branch_1 = concatenate([branch_10, branch_11], axis=channel_axis)
+
+
+    branch_2 = conv2d_bn(input, 384, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 448, 3, 1)
+    branch_2 = conv2d_bn(branch_2, 512, 1, 3)
+    branch_20 = conv2d_bn(branch_2, 256, 1, 3)
+    branch_21 = conv2d_bn(branch_2, 256, 3, 1)
+    branch_2 = concatenate([branch_20, branch_21], axis=channel_axis)
+
+    branch_3 = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(input)
+    branch_3 = conv2d_bn(branch_3, 256, 1, 1)
+
+    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
+    return x
+
+
+def inception_v4_base(input):
+    channel_axis = -1
+
+    # Input Shape is 299 x 299 x 3 (th) or 3 x 299 x 299 (th)
+    net = conv2d_bn(input, 32, 3, 3, strides=(2,2), padding='valid')
+    net = conv2d_bn(net, 32, 3, 3, padding='valid')
+    net = conv2d_bn(net, 64, 3, 3)
+
+    branch_0 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(net)
+
+    branch_1 = conv2d_bn(net, 96, 3, 3, strides=(2,2), padding='valid')
+
+    net = concatenate([branch_0, branch_1], axis=channel_axis)
+
+    branch_0 = conv2d_bn(net, 64, 1, 1)
+    branch_0 = conv2d_bn(branch_0, 96, 3, 3, padding='valid')
+
+    branch_1 = conv2d_bn(net, 64, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 64, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 64, 7, 1)
+    branch_1 = conv2d_bn(branch_1, 96, 3, 3, padding='valid')
+
+    net = concatenate([branch_0, branch_1], axis=channel_axis)
+
+    branch_0 = conv2d_bn(net, 192, 3, 3, strides=(2,2), padding='valid')
+    branch_1 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(net)
+
+    net = concatenate([branch_0, branch_1], axis=channel_axis)
+
+    # 35 x 35 x 384
+    # 4 x Inception-A blocks
+    for idx in range(4):
+    	net = block_inception_a(net)
+
+    # 35 x 35 x 384
+    # Reduction-A block
+    net = block_reduction_a(net)
+
+    # 17 x 17 x 1024
+    # 7 x Inception-B blocks
+    for idx in range(7):
+    	net = block_inception_b(net)
+
+    # 17 x 17 x 1024
+    # Reduction-B block
+    net = block_reduction_b(net)
+
+    # 8 x 8 x 1536
+    # 3 x Inception-C blocks
+    for idx in range(3):
+    	net = block_inception_c(net)
+
+    return net
+
+
+def inception_v4(num_classes, dropout_keep_prob, weights, include_top):
+    '''
+    Creates the inception v4 network
+    Args:
+    	num_classes: number of classes
+    	dropout_keep_prob: float, the fraction to keep before final layer.
+
+    Returns:
+    	logits: the logits outputs of the model.
+    '''
+
+    # Input Shape is 128 x 128 x 3 (tf) or 3 x 128 x 128 (th)
+    inputs = Input((128, 128, 3))
+
+    # Make inception base
+    x = inception_v4_base(inputs)
+
+
+    # Final pooling and prediction
+    x = AveragePooling2D((2,2), padding='valid')(x)
+    x = Dropout(dropout_keep_prob)(x)
+
+    x = Flatten()(x)
+    x = Dense(units=800, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(units=100, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(units=num_classes, activation='sigmoid')(x)
+
+    model = Model(inputs, x, name='inception_v4')
+    return model
+
+
+def create_model(num_classes=1, dropout_prob=0.2, weights=None, include_top=True):
+    return inception_v4(num_classes, dropout_prob, weights, include_top)
+
+
+model = create_model()
+#model = load_model('InceptionModel.h5')
+#compile
+model.compile(loss='binary_crossentropy',
+            optimizer = keras.optimizers.SGD(lr=0.03, decay=0, momentum=0, nesterov=False),
+            metrics=['acc'])
+
+#define early stopping
+Val_loss_increased = EarlyStopping(monitor='val_loss', mode = 'min', verbose=1, patience=8)
+Train_NotImproved = EarlyStopping(monitor='acc', mode = 'max', verbose=1, baseline = 0.3, patience = 5)
+mc = ModelCheckpoint('Inception_1.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+#fit
+history = model.fit(X_train, Y_train, batch_size = 2315,epochs = 50,verbose = 1, validation_data = (X_CV,Y_CV),shuffle=True, callbacks=[Val_loss_increased, Train_NotImproved,mc])
+
+###save model
+##model.save('Inception_1.h5')
+
+#evaluate
+preds = model.evaluate(X_CV, Y_CV)
+
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+#summary
+model.summary()
+
+#model history
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
