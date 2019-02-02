6a29046364aa6c36574d773bea87ce485c9742be
apple <apple@appledeMacBook-Pro.local>
2019-02-02 20:40:54 +0100

    prediction based on resnet

--- ModelPredict.py
+++ ModelPredict.py
@@ -2,12 +2,18 @@ import numpy as np
 import cv2
 from keras.models import Model,load_model
 
-model = load_model('ResNet50.h5')
+input_image = cv2.imread('Input.png')
+input_image = np.expand_dims(input_image,axis=0)
+input_image = input_image/255.
+model = load_model('ResNet50_8.h5')
 #model.get_weigts()
-input_image = cv2.imread('InputImage.jpg')
 
 def predict(img):
     pred = model.predict(img)
+    if pred <= 0.5:
+        pred = 0
+    else:
+        pred = 1
     print("Prediction of the input image: " + str(pred))
     return pred
 
