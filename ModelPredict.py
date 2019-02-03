e1a8dd4f740027fc9cafeee9f995bebeb55ab02b
apple <apple@appledeMacBook-Pro.local>
2019-02-03 23:58:37 +0100

    model predict set up for inception model

--- ModelPredict.py
+++ ModelPredict.py
@@ -1,20 +1,25 @@
+from keras.models import Model,load_model
 import numpy as np
 import cv2
-from keras.models import Model,load_model
 
-input_image = cv2.imread('Input.png')
-input_image = np.expand_dims(input_image,axis=0)
-input_image = input_image/255.
-model = load_model('ResNet50_8.h5')
-#model.get_weigts()
+img = cv2.imread('image.png')
+img = np.expand_dims(img,axis=0)
+img = img/255.
+
+#ResNet = load_model('ResNet50_8.h5')
+InceptionModel = load_model('Inception_2.h5')
 
 def predict(img):
-    pred = model.predict(img)
-    if pred <= 0.5:
-        pred = 0
+    #pred_res = ResNet.predict(img)
+    pred_inc = InceptionModel.predict(img)
+    print(pred_inc)
+    #pred = "mix"
+    if pred_inc <= 0.5:
+        pred_inc = 0
     else:
-        pred = 1
-    print("Prediction of the input image: " + str(pred))
-    return pred
+        pred_inc = 1
+    print("Prediction of the input image by ResNet: " + str(pred_inc))
 
-prediction = predict(input_image)
+    return pred_inc
+
+prediction = predict(img)
