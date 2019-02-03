6ef6034bc27064f5ace7a1a12d341a55515935c2
apple <apple@appledeMacBook-Pro.local>
2019-02-03 22:36:52 +0100

    viewed another model

--- ModelArchitecturePreview.py
+++ ModelArchitecturePreview.py
@@ -1,225 +1,198 @@
+#import all necessary libraries
 import numpy as np
-# Sys
-import warnings
-# Keras Core
-from keras.layers.convolutional import MaxPooling2D, Convolution2D, AveragePooling2D
-from keras.layers import Input, Dropout, Dense, Flatten, Activation
-from keras.layers.normalization import BatchNormalization
-from keras.layers.merge import concatenate
-from keras import regularizers
-from keras import initializers
-from keras.models import Model
-# Backend
-from keras import backend as K
-# Utils
-from keras.utils.layer_utils import convert_all_kernels_in_model
+import keras
+from keras.losses import binary_crossentropy
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, Dropout
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+#from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from resnets_utils import *
+from keras.initializers import glorot_uniform
+import scipy.misc
+from matplotlib.pyplot import imshow
+from matplotlib import pyplot as plt
+import keras.backend as K
+from time import sleep
+import cv2
 
 #create your model
-
-def conv2d_bn(x, nb_filter, num_row, num_col,
-              padding='same', strides=(1, 1), use_bias=False):
-    channel_axis = -1
-    x = Convolution2D(nb_filter, (num_row, num_col),
-                      strides=strides,
-                      padding=padding,
-                      use_bias=use_bias,
-                      kernel_regularizer=regularizers.l2(0.00004),
-                      kernel_initializer=initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='normal', seed=None))(x)
-    x = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(x)
-    x = Activation('relu')(x)
-    return x
-
-
-def block_inception_a(input):
-    channel_axis = -1
-
-    branch_0 = conv2d_bn(input, 96, 1, 1)
-
-    branch_1 = conv2d_bn(input, 64, 1, 1)
-    branch_1 = conv2d_bn(branch_1, 96, 3, 3)
-
-    branch_2 = conv2d_bn(input, 64, 1, 1)
-    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
-    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
-
-    branch_3 = AveragePooling2D((3,3), strides=(1,1), padding='same')(input)
-    branch_3 = conv2d_bn(branch_3, 96, 1, 1)
-
-    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
-    return x
-
-
-def block_reduction_a(input):
-    channel_axis = -1
-
-    branch_0 = conv2d_bn(input, 384, 3, 3, strides=(2,2), padding='valid')
-
-    branch_1 = conv2d_bn(input, 192, 1, 1)
-    branch_1 = conv2d_bn(branch_1, 224, 3, 3)
-    branch_1 = conv2d_bn(branch_1, 256, 3, 3, strides=(2,2), padding='valid')
-
-    branch_2 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(input)
-
-    x = concatenate([branch_0, branch_1, branch_2], axis=channel_axis)
-    return x
-
-
-def block_inception_b(input):
-    channel_axis = -1
-
-    branch_0 = conv2d_bn(input, 384, 1, 1)
-
-    branch_1 = conv2d_bn(input, 192, 1, 1)
-    branch_1 = conv2d_bn(branch_1, 224, 1, 7)
-    branch_1 = conv2d_bn(branch_1, 256, 7, 1)
-
-    branch_2 = conv2d_bn(input, 192, 1, 1)
-    branch_2 = conv2d_bn(branch_2, 192, 7, 1)
-    branch_2 = conv2d_bn(branch_2, 224, 1, 7)
-    branch_2 = conv2d_bn(branch_2, 224, 7, 1)
-    branch_2 = conv2d_bn(branch_2, 256, 1, 7)
-
-    branch_3 = AveragePooling2D((3,3), strides=(1,1), padding='same')(input)
-    branch_3 = conv2d_bn(branch_3, 128, 1, 1)
-
-    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
-    return x
-
-
-def block_reduction_b(input):
-    channel_axis = -1
-
-    branch_0 = conv2d_bn(input, 192, 1, 1)
-    branch_0 = conv2d_bn(branch_0, 192, 3, 3, strides=(2, 2), padding='valid')
-
-    branch_1 = conv2d_bn(input, 256, 1, 1)
-    branch_1 = conv2d_bn(branch_1, 256, 1, 7)
-    branch_1 = conv2d_bn(branch_1, 320, 7, 1)
-    branch_1 = conv2d_bn(branch_1, 320, 3, 3, strides=(2,2), padding='valid')
-
-    branch_2 = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(input)
-
-    x = concatenate([branch_0, branch_1, branch_2], axis=channel_axis)
-    return x
-
-
-def block_inception_c(input):
-    channel_axis = -1
-
-    branch_0 = conv2d_bn(input, 256, 1, 1)
-
-    branch_1 = conv2d_bn(input, 384, 1, 1)
-    branch_10 = conv2d_bn(branch_1, 256, 1, 3)
-    branch_11 = conv2d_bn(branch_1, 256, 3, 1)
-    branch_1 = concatenate([branch_10, branch_11], axis=channel_axis)
-
-
-    branch_2 = conv2d_bn(input, 384, 1, 1)
-    branch_2 = conv2d_bn(branch_2, 448, 3, 1)
-    branch_2 = conv2d_bn(branch_2, 512, 1, 3)
-    branch_20 = conv2d_bn(branch_2, 256, 1, 3)
-    branch_21 = conv2d_bn(branch_2, 256, 3, 1)
-    branch_2 = concatenate([branch_20, branch_21], axis=channel_axis)
-
-    branch_3 = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(input)
-    branch_3 = conv2d_bn(branch_3, 256, 1, 1)
-
-    x = concatenate([branch_0, branch_1, branch_2, branch_3], axis=channel_axis)
-    return x
-
-
-def inception_v4_base(input):
-    channel_axis = -1
-
-    # Input Shape is 299 x 299 x 3 (th) or 3 x 299 x 299 (th)
-    net = conv2d_bn(input, 32, 3, 3, strides=(2,2), padding='valid')
-    net = conv2d_bn(net, 32, 3, 3, padding='valid')
-    net = conv2d_bn(net, 64, 3, 3)
-
-    branch_0 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(net)
-
-    branch_1 = conv2d_bn(net, 96, 3, 3, strides=(2,2), padding='valid')
-
-    net = concatenate([branch_0, branch_1], axis=channel_axis)
-
-    branch_0 = conv2d_bn(net, 64, 1, 1)
-    branch_0 = conv2d_bn(branch_0, 96, 3, 3, padding='valid')
-
-    branch_1 = conv2d_bn(net, 64, 1, 1)
-    branch_1 = conv2d_bn(branch_1, 64, 1, 7)
-    branch_1 = conv2d_bn(branch_1, 64, 7, 1)
-    branch_1 = conv2d_bn(branch_1, 96, 3, 3, padding='valid')
-
-    net = concatenate([branch_0, branch_1], axis=channel_axis)
-
-    branch_0 = conv2d_bn(net, 192, 3, 3, strides=(2,2), padding='valid')
-    branch_1 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(net)
-
-    net = concatenate([branch_0, branch_1], axis=channel_axis)
-
-    # 35 x 35 x 384
-    # 4 x Inception-A blocks
-    for idx in range(4):
-    	net = block_inception_a(net)
-
-    # 35 x 35 x 384
-    # Reduction-A block
-    net = block_reduction_a(net)
-
-    # 17 x 17 x 1024
-    # 7 x Inception-B blocks
-    for idx in range(7):
-    	net = block_inception_b(net)
-
-    # 17 x 17 x 1024
-    # Reduction-B block
-    net = block_reduction_b(net)
-
-    # 8 x 8 x 1536
-    # 3 x Inception-C blocks
-    for idx in range(3):
-    	net = block_inception_c(net)
-
-    return net
-
-
-def inception_v4(num_classes, dropout_keep_prob, weights, include_top):
-    '''
-    Creates the inception v4 network
-    Args:
-    	num_classes: number of classes
-    	dropout_keep_prob: float, the fraction to keep before final layer.
+def identity_block(X, f, filters, stage, block):
+    """
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
 
     Returns:
-    	logits: the logits outputs of the model.
-    '''
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
 
-    # Input Shape is 128 x 128 x 3 (tf) or 3 x 128 x 128 (th)
-    inputs = Input((128, 128, 3))
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    # Make inception base
-    x = inception_v4_base(inputs)
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value. You'll need this later to add back to the main path.
+    X_shortcut = X
+
+    # First component of main path
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters = F2, kernel_size = (f,f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X,X_shortcut])
+    X = Activation('relu')(X)
+    return X
+
+#implement convolution block which is also a risidual block but this one change the size of input data
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    """
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value
+    X_shortcut = X
 
 
-    # Final pooling and prediction
-    x = AveragePooling2D((2,2), padding='valid')(x)
-    x = Dropout(dropout_keep_prob)(x)
-    
-    x = Flatten()(x)
-    x = Dense(units=800, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(units=100, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(units=num_classes, activation='sigmoid')(x)
+    ##### MAIN PATH #####
+    # First component of main path
+    X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
 
-    model = Model(inputs, x, name='inception_v4')
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(F2, (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(F3, (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    ##### SHORTCUT PATH #### (≈2 lines)
+    X_shortcut = Conv2D(F3, (1, 1), strides = (s,s), name = conv_name_base + '1')(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X,X_shortcut])
+    X = Activation('relu')(X)
+    X = Dropout(0.1)(X)
+
+    return X
+
+#create resnet with 50 layers using the risidual blocks implemented above
+def ResNet50(input_shape,classes):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((7, 7))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (5, 5), strides = (2, 2), name = 'conv1', padding ='same')(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2,2))(X)
+    X = Dropout(0.1)(X)
+
+    # Stage 2
+    X = Conv2D(128, (3, 3), strides = (2, 2), padding = 'same', name = 'conv2')(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv2')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)
+    X = Dropout(0.1)(X)
+
+    # Stage 3
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=3, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=3, block='c')
+
+    # Stage 4
+    X = convolutional_block(X, f = 3, filters = [128,128,512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128,128,512], stage=4, block='b')
+    X = identity_block(X, 3, [128,128,512], stage=4, block='c')
+    X = identity_block(X, 3, [128,128,512], stage=4, block='d')
+
+    # Stage 5
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=5, block='f')
+
+    # Stage 6
+    X = convolutional_block(X, f = 3, filters = [612, 612, 2248], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [612, 612, 2248], stage=6, block='b')
+    X = identity_block(X, 3, [612, 612, 2248], stage=6, block='c')
+
+    # Maxpool
+    X = MaxPooling2D(pool_size=(2,2), name = 'max_pool')(X)
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(1000, activation='relu', name='fc0' + str(classes))(X)
+    X = Dropout(0.5)(X)
+    X = Dense(100, activation='relu', name='fc2' + str(classes))(X)
+    X = Dropout(0.5)(X)
+    X = Dense(1, activation='sigmoid', name='fc3' + str(classes))(X)
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
     return model
 
+#create model
+model = ResNet50((128,128,3),1)
 
-def create_model(num_classes=1, dropout_prob=0.2, weights=None, include_top=True):
-    return inception_v4(num_classes, dropout_prob, weights, include_top)
 
-
-model = create_model()
 print(model.summary())
